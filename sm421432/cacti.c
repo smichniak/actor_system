9dbc4a6b2e8a55c16d63822a4688eb311bfee807
Szymon Michniak <s.michniak@icloud.com>
2021-01-19 23:28:23 +0100

    Template

---
+++ sm421432/cacti.c
@@ -0,0 +1,15 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+// TODO Optimize includes
+
+
+#include "cacti.h"
+
